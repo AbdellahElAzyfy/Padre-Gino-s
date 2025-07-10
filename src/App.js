0f62765aff5e771ee5f922ba90ae53d478ca9f31
Abdellah El azyfy <abdallahelazyfy47@gmail.com>
2025-07-10 13:18:31 +0100

    Initial Padre Gino's projet setup

---
+++ src/App.js
@@ -0,0 +1,30 @@
+function Pizza({ name, description }) {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, name),
+    React.createElement("p", {}, description),
+  ]);
+}
+
+function App() {
+  return React.createElement(
+    "div",
+    {},
+    React.createElement("h1", {}, "Pixel Perfect Pizzas"),
+    React.createElement(Pizza, {
+      name: "The Pepperoni Pizza",
+      description: "Mozzarella Cheese, Pepperoni",
+    }),
+    React.createElement(Pizza, {
+      name: "The Big Meat Pizza",
+      description: "Bacon, Pepperoni, Italian Sausage, Chorizo Sausage",
+    }),
+    React.createElement(Pizza, {
+      name: "The Hawaiian Pizza",
+      description: "Sliced Ham, Pineapple, Mozzarella Cheese",
+    }),
+  );
+}
+
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App));
